17811d0a8488f402fcf4bc56744a5d0b53862f5a
AliEmad2002 <99054912+AliEmad2002@users.noreply.github.com>
2023-03-26 21:32:45 +0200

    Revert "minor"
    This reverts commit 1b74932ae3752cca39321164beac25f136280136.

--- Src/Blocked_List.c
+++ Src/Blocked_List.c
@@ -22,7 +22,7 @@
 
 
 /*	allocate links statically	*/
-static RTOS_TCB_t* blockedTcbPtrArr[RTOS_MAX_NUMBER_OF_TCBS] = {NULL};
+static RTOS_BlockedElement_t blockedArr[RTOS_MAX_NUMBER_OF_TCBS] = {0};
 
 /*******************************************************************************
  * Adding TCB (pointer) to the blocked list:
@@ -31,11 +31,12 @@ void RTOS_Blocked_List_voidAdd(RTOS_TCB_t* tcbPtr)
 {
 	/*	find index of the first non used element in "blockedArr":	*/
 	u8 i = 0;
-	while(blockedTcbPtrArr[i] != NULL)
+	while(blockedArr[i].isUsed)
 		i++;
 
 	/*	add at it	*/
-	blockedTcbPtrArr[i] = tcbPtr;
+	blockedArr[i].isUsed = true;
+	blockedArr[i].tcbPtr = tcbPtr;
 }
 
 /*******************************************************************************
@@ -51,10 +52,10 @@ b8 RTOS_Blocked_List_b8Unblock(RTOS_TCB_t** distP)
 	for (u8 i = 0; i < RTOS_MAX_NUMBER_OF_TCBS; i++)
 	{
 		/*	if there's a blocked thread at this index:	*/
-		while(blockedTcbPtrArr[i] != NULL)
+		if (blockedArr[i].isUsed)
 		{
 			/*	get pointer of this TCB	*/
-			RTOS_TCB_t* tcbPtr = blockedTcbPtrArr[i];
+			RTOS_TCB_t* tcbPtr = blockedArr[i].tcbPtr;
 
 			/*	if it was time blocked:	*/
 			if (tcbPtr->blockingReason == RTOS_TCB_BlockingReason_Time)
@@ -62,7 +63,7 @@ b8 RTOS_Blocked_List_b8Unblock(RTOS_TCB_t** distP)
 				/*	if ready time has come	*/
 				if (tcbPtr->targetReadyTime <= RTOS_Scheduler_u64GetSystemTime())
 				{
-					blockedTcbPtrArr[i] = NULL;
+					blockedArr[i].isUsed = false;
 					tcbPtr->isBlocked = false;
 					*distP = tcbPtr;
 					return true;
