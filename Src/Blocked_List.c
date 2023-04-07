f0801f2a9e7c14dc362152ac6191cb335a4ff186
AliEmad2002 <ali.20352473@eng.aun.edu.eg>
2023-04-07 22:49:39 +0200

    added mutex timeout handling

--- Src/Blocked_List.c
+++ Src/Blocked_List.c
@@ -51,36 +51,74 @@ b8 RTOS_Blocked_List_b8Unblock(RTOS_TCB_t** distP)
 	 */
 	for (u8 i = 0; i < RTOS_MAX_NUMBER_OF_TCBS; i++)
 	{
-		/*	if there's a blocked thread at this index:	*/
-		if (blockedTcbPtrArr[i] != NULL)
-		{
-			/*	get pointer of this TCB	*/
-			RTOS_TCB_t* tcbPtr = blockedTcbPtrArr[i];
+		/*	if there isn't a blocked thread at this index, continue	*/
+		if (blockedTcbPtrArr[i] == NULL)
+			continue;
 
-			/*	if it was time blocked:	*/
-			if (tcbPtr->blockingReason == RTOS_TCB_BlockingReason_Time)
+		/*	otherwise, get pointer of this blocked TCB	*/
+		RTOS_TCB_t* tcbPtr = blockedTcbPtrArr[i];
+
+		/*	if it was time blocked:	*/
+		if (tcbPtr->blockingReason == RTOS_TCB_BlockingReason_Time)
+		{
+			/*	if ready time has come	*/
+			if (tcbPtr->targetReadyTime <= RTOS_Scheduler_u64GetSystemTime())
 			{
-				/*	if ready time has come	*/
-				if (tcbPtr->targetReadyTime <= RTOS_Scheduler_u64GetSystemTime())
-				{
-					blockedTcbPtrArr[i] = NULL;
-					tcbPtr->isBlocked = false;
-					*distP = tcbPtr;
-					return true;
-				}
+				/*	un-block TCB	*/
+				blockedTcbPtrArr[i] = NULL;
+				tcbPtr->isBlocked = false;
+				*distP = tcbPtr;
+				return true;
+			}
+		}
+
+		/*	otherwise, if blocking reason was mutex or  semaphore	*/
+		else
+		{
+			/**
+			 * since this function is only called in scheduler, which is a
+			 * critical section, mutex / semaphore can be accessed here with
+			 * guarantee that it is not being accessed from any other thread or
+			 * handler.
+			 **/
+
+			/*	if mutex / semaphore has beecome available	*/
+			if (*(tcbPtr->mutexPtr))
+			{
+				/*	take it	*/
+				(*(tcbPtr->mutexPtr))--;
+
+				/*
+				 * this is checked by "mutex_take()", when it is "NULL", itt means
+				 * successfully taken within the given timeout.
+				 */
+				tcbPtr->mutexPtr = NULL;
+
+				/*	un-block TCB	*/
+				blockedTcbPtrArr[i] = NULL;
+				tcbPtr->isBlocked = false;
+				*distP = tcbPtr;
+				return true;
 			}
 
-			/*	otherwise, if blocking reason was mutex or  semaphore	*/
+			/*	otherwise, if not yet available	*/
 			else
 			{
-				if (*(tcbPtr->mutexPtr))
+				/*	if timeout has come	*/
+				if (tcbPtr->targetReadyTime <= RTOS_Scheduler_u64GetSystemTime())
 				{
-					(*(tcbPtr->mutexPtr))--;
+					/*	un-block TCB	*/
 					blockedTcbPtrArr[i] = NULL;
 					tcbPtr->isBlocked = false;
-					tcbPtr->mutexPtr = NULL;
 					*distP = tcbPtr;
 					return true;
+
+					/**
+					 * When this thread continues in "mutex_take()", it will check
+					 * value of "tcbPtr->mutexPtr". since it has not been changed
+					 * to "NULL", it means  that mutex / semaphore has not been
+					 * taken in the given timeout.
+					 **/
 				}
 			}
 		}
