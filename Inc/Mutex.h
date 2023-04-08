a57f995f50727510561c69300f0e390fa77048ab
AliEmad2002 <ali.20352473@eng.aun.edu.eg>
2023-04-08 15:42:32 +0200

    Update Mutex.h

--- Inc/Mutex.h
+++ Inc/Mutex.h
@@ -13,8 +13,6 @@
  * "timeOut" is number of RTOS ticks to wait if the mutex / semaphore was not
  * available.
  *
- * For infinite wait: "timeOut" = 0
- *
  * returns 1 if mutex / semaphore was successfully taken, 0 otherwwise.
  */
 u8 RTOS_Mutex_u8Take(u8* mutexPtr, u32 timeOut);
